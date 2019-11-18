efca909e916a8bc0d78f76de9b129e7ec001e723
Martha <838021004@qq.com>
2019-11-18 19:43:38 +0800

    basic GUI

--- WinReg_wzk/winRegWzk.py
+++ WinReg_wzk/winRegWzk.py
@@ -1,10 +1,17 @@
 import winreg
+import tkinter as tk
 
 
 class WinReg(object):
-    def __init__(self):
-        self.hkey = winreg.HKEY_CLASSES_ROOT
-        self.bootPath = r'SOFTWARE\Microsoft\Windows\CurrentVersion\Run'
+    def __init__(self, mytop):
+        self.top = mytop
+        self.key_entry = tk.Entry(self.top, width=100)
+        self.valueName_entry = tk.Entry(self.top, width=100)
+        self.value_entry = tk.Entry(self.top, width=100)
+        self.result_text = tk.Text(self.top, width=100)
+        self.hkeyRoot = winreg.HKEY_CLASSES_ROOT
+        self.hkey = winreg.HKEY_LOCAL_MACHINE
+        self.bootPath = r'SOFTWARE\Microsoft\Windows\CurrentVersion\Run'    # HKEY_LOCAL_MACHINE
         self.txtPath = r'.txt\ShellNew'    # HKEY_CLASSES_ROOT\.txt\ShellNew
         self.txtBound = r'@%SystemRoot%\system32\notepad.exe,-470'
 
@@ -12,70 +19,111 @@ class WinReg(object):
         return winreg.OpenKeyEx(self.hkey, key, access=access)
 
     def txtCheck(self):
-        key = self.openKey(self.txtPath)
+        key = winreg.OpenKeyEx(self.hkeyRoot, self.txtPath, access=winreg.KEY_ALL_ACCESS)
+        # key = self.openKey(self.txtPath)
         if winreg.QueryValueEx(key, 'ItemName')[0] == self.txtBound:
-            print("ok")
+            self.result_text.insert(100.100, "Txt opened by notepad.exe")
         else:
-            print("no", winreg.QueryValueEx(key, 'ItemName')[0])
+            # print("no", winreg.QueryValueEx(key, 'ItemName')[0])
+            self.result_text.insert(100.100, "Txt open:{}".format(winreg.QueryValueEx(key, 'ItemName')[0]))
 
-    def addReg(self, key, subKey):
-        key = self.openKey(key)
-        newReg = winreg.CreateKeyEx(key, subKey)
-        return newReg
+    # def addReg(self, key, subKey):
+    #     key = self.openKey(key)
+    #     newReg = winreg.CreateKeyEx(key, subKey)
+    #     return newReg
 
-    def deleteReg(self, key, subKey):
-        key = self.openKey(key)
+    # def deleteReg(self, key, subKey):
+    #     key = self.openKey(key)
+    #     try:
+    #         winreg.DeleteKeyEx(key, subKey)
+    #         return True
+    #     except OSError as e:
+    #         return e
+
+    def addValue(self):
+        key = self.key_entry.get()
+        valueName = self.valueName_entry.get()
+        value = self.value_entry.get()
+        okey = self.openKey(key, access=winreg.KEY_ALL_ACCESS)    # open key
         try:
-            winreg.DeleteKeyEx(key, subKey)
-            return True
+            winreg.SetValueEx(okey, valueName, 0, winreg.REG_SZ, value)
+            self.result_text.insert(100.100, "Value Added\n")
         except OSError as e:
-            return e
+            self.result_text.insert(100.100, "{}\n".format(e))
 
-    def addValue(self, key, valueName, value):
-        key = self.openKey(key, access=winreg.KEY_ALL_ACCESS)
+    def deleteValue(self):
+        key = self.key_entry.get()
+        valueName = self.valueName_entry.get()
+        okey = self.openKey(key, access=winreg.KEY_SET_VALUE)
         try:
-            winreg.SetValueEx(key, valueName, 0, winreg.REG_SZ, value)
-            return True
+            winreg.DeleteValue(okey, valueName)
+            self.result_text.insert(100.100, "Value Deleted\n")
         except OSError as e:
-            print("in add:", e)
-            return e
+            self.result_text.insert(100.100, "{}\n".format(e))
 
-    def deleteValue(self, key, valueName):
-        key = self.openKey(key, access=winreg.KEY_SET_VALUE)
-        try:
-            winreg.DeleteValue(key, valueName)
-            return True
-        except OSError as e:
-            print("delete:", e)
-            return e
-
-    def checkValue(self, key, valueName=None):
-        key = self.openKey(key)
-        if valueName is None:
+    def checkValue(self):
+        key = self.key_entry.get()
+        valueName = self.valueName_entry.get()
+        okey = self.openKey(key)
+        if valueName == '':    # no input
             i = 0
             while True:
                 try:
-                    value = winreg.EnumValue(key, i)
-                    print("enum value", value)
+                    value = winreg.EnumValue(okey, i)
+                    self.result_text.insert(100.100, "{}\n".format(value))
                     i += 1
                 except OSError:
-                    return
+                    self.result_text.insert(100.100, "End\n")
+                    break
         else:
-            value = winreg.QueryValueEx(key, valueName)
-            print("query value", value)
+            value = winreg.QueryValueEx(okey, valueName)
+            self.result_text.insert(100.100, "{}\n".format(value))
 
-    def updateValue(self, key, valueName, value):
-        key = self.openKey(key, access=winreg.KEY_SET_VALUE)
+    def updateValue(self):
+        key = self.key_entry.get()
+        valueName = self.valueName_entry.get()
+        value = self.value_entry.get()
+        okey = self.openKey(key, access=winreg.KEY_SET_VALUE)
         try:
-            winreg.SetValueEx(key, valueName, 0, winreg.REG_SZ, value)
-            print("updated")
+            winreg.SetValueEx(okey, valueName, 0, winreg.REG_SZ, value)
+            self.result_text.insert(100.100, "Value Updated\n")
         except OSError as e:
-            print("in update:", e)
-            return e
+            self.result_text.insert(100.100, "{}\n".format(e))
+    
+    def window(self):
+        self.top.title('RegTool       by Echo Wang')
+        tk.Label(self.top, text='RegTool       by Echo Wang')
+        tk.Label(self.top, text='Key').grid(row=1)
+        tk.Label(self.top, text='Value Name').grid(row=3)
+        tk.Label(self.top, text='Value').grid(row=5)
+        tk.Label(self.top, text='Result').grid(row=7)
+        # tk.Label(self.top, text='Close port').grid(row=6)
+        self.key_entry.grid(row=1, column=1, columnspan=5)
+        self.valueName_entry.grid(row=3, column=1, columnspan=5)
+        self.value_entry.grid(row=5, column=1, columnspan=5)
+        self.result_text.grid(row=7, column=1, columnspan=5)
+        addBtn = tk.Button(self.top, text='Add Value',
+                           command=self.addValue)
+        addBtn.grid(row=6, column=1)
+        deleteBtn = tk.Button(self.top, text='Delete Value',
+                              command=self.deleteValue)
+        deleteBtn.grid(row=6, column=2)
+        checkBtn = tk.Button(self.top, text='Check Value',
+                             command=self.checkValue)
+        checkBtn.grid(row=6, column=3)
+        updateBtn = tk.Button(self.top, text='Update Value',
+                              command=self.updateValue)
+        updateBtn.grid(row=6, column=4)
+        txtCheckBtn = tk.Button(self.top, text='Txt Check',
+                                command=self.txtCheck)
+        txtCheckBtn.grid(row=6, column=5)
 
 
 if __name__ == "__main__":
-    myReg = WinReg()
+    top = tk.Tk()
+    myReg = WinReg(top)
+    myReg.window()
+    top.mainloop()
     # myReg.addValue(myReg.bootPath, 'test', 'value')
     # myReg.addValue('MyReg', 'test', 'value')
     # myReg.deleteValue(myReg.bootPath, 'test')
@@ -83,4 +131,4 @@ if __name__ == "__main__":
     # myReg.checkValue(myReg.bootPath)
     # print("000")
     # myReg.checkValue(myReg.bootPath, 'test')
-    myReg.txtCheck()
+    # myReg.txtCheck()
